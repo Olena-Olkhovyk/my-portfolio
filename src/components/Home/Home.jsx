28146b689004086ba0fb7b67b867a46073d6c877
Olena-Olkhovyk <alyonaeremen@gmail.com>
2024-02-07 21:57:29 +0100

    Move home comp files to components folder

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,8 @@
+import { FaLinkedin } from "react-icons/fa";
+import profileImg from "../../assets/home/olena.png";
+import shape1 from "../../assets/home/shape-1.png";
+import shape2 from "../../assets/home/shape-2.png";
+const Home = () => {
+  return <h1>from home</h1>;
+};
+export default Home;
