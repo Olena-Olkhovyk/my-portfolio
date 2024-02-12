b42e33feaba6a71937942350d86c83a1266d20f5
Olena-Olkhovyk <alyonaeremen@gmail.com>
2024-02-12 19:27:19 +0100

    Add data for services component

---
+++ src/assets/data/data.js
@@ -0,0 +1,23 @@
+export const services = [
+  {
+    name: "frontend development",
+    title: "websites development",
+    description:
+      "I create eye-catching, functional and responsive interfaces for different types of sites.",
+  },
+  {
+    name: "mentoring",
+    title: "html, css, javaScript, react",
+    description: "I help newcomers in the IT field to develop websites",
+  },
+  {
+    name: "mentoring",
+    title: "english language",
+    description: "I help newcomers learn the basics of English ",
+  },
+  {
+    name: "mentoring",
+    title: "bug fixing",
+    description: "I help SheCode's students fix bugs",
+  },
+];
