fdfceb917146d20f701fdf523eac61d89ac4ad7d
Olena-Olkhovyk <alyonaeremen@gmail.com>
2024-02-12 15:15:14 +0100

    Include services component in app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Home from "./components/Home/Home";
+import Services from "./components/Services/Services";
 
 function App() {
   return (
     <main className="main">
       <Home />
+      <Services />
     </main>
   );
 }
