b12eb031ea1d72be46b0dcfa0d890bd9ec8a0f27
Olena-Olkhovyk <alyonaeremen@gmail.com>
2024-02-13 13:55:24 +0100

    Include skills component to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Home from "./components/Home/Home";
 import Services from "./components/Services/Services";
+import Skills from "./components/Skills/Skills";
 
 function App() {
   return (
     <main className="main">
       <Home />
       <Services />
+      <Skills />
     </main>
   );
 }
