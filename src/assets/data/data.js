f6b429da61a4be0e89e449578b822783fbd9cd11
Olena-Olkhovyk <alyonaeremen@gmail.com>
2024-02-13 13:55:40 +0100

    Add data to skills somponent

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -21,3 +21,42 @@ export const services = [
     description: "I help SheCode's students fix bugs",
   },
 ];
+
+export const skills = [
+  {
+    id: 1,
+    name: "HTML",
+    percentage: 95,
+    description: "lorem204444444444ddddddddddddddddfvcdedcecd",
+  },
+  {
+    id: 2,
+    name: "CSS",
+    percentage: 95,
+    description: "lorem204dddddddddfvcdedcecd",
+  },
+  {
+    id: 3,
+    name: "JavaScript",
+    percentage: 90,
+    description: "lorem20444444444444ddddddddfvcdedcecd",
+  },
+  {
+    id: 4,
+    name: "React",
+    percentage: 90,
+    description: "lorem204444444444dddddddfvcdedcecd",
+  },
+  {
+    id: 5,
+    name: "Git",
+    percentage: 90,
+    description: "lorem2044444444444ddddddddddddddddfvcdedcecd",
+  },
+  {
+    id: 6,
+    name: "FIGMA",
+    percentage: 95,
+    description: "lorem2044444444dddddddddddddfvcdedcecd",
+  },
+];
