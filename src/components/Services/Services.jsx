f9fdbf3b3b79b1ec4806ccbc8724e5cb5e62aa48
Olena-Olkhovyk <alyonaeremen@gmail.com>
2024-02-14 21:00:47 +0100

    Change background color in services and skills comppnents

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -9,48 +9,50 @@ import { Pagination } from "swiper/modules";
 const Services = () => {
   const container = "container";
   return (
-    <section className={`${container} ${classes.services}`} id="services">
-      <h2 className="sectionTitle">What I Do</h2>
-      <p className="sectionSubtitle">
-        My <span>Services</span>
-      </p>
+    <div className={classes.servicesPage}>
+      <section className={`${container} ${classes.services}`} id="services">
+        <h2 className="sectionTitle">What I Do</h2>
+        <p className="sectionSubtitle">
+          My <span>Services</span>
+        </p>
 
-      <Swiper
-        pagination={{
-          clickable: true,
-        }}
-        breakpoints={{
-          540: {
-            slidesPerView: 1,
-            spaceBetween: 30,
-          },
-          768: {
-            slidesPerView: 2,
-            spaceBetween: 30,
-          },
-          1024: {
-            slidesPerView: 3,
-            spaceBetween: 40,
-          },
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        {services.map(({ name, title, description }, index) => (
-          <SwiperSlide className={classes.servicesCard} key={index}>
-            <span className={classes.servicesSubtitle}>{name}</span>
-            <h3 className={classes.servicesTitle}>{title}</h3>
-            <p className={classes.servicesDescription}>{description}</p>
-            <h4 className={classes.free}>free</h4>
-            <img
-              src={shapeTwo}
-              alt="shape image"
-              className={classes.servicesShape}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </section>
+        <Swiper
+          pagination={{
+            clickable: true,
+          }}
+          breakpoints={{
+            540: {
+              slidesPerView: 1,
+              spaceBetween: 30,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 30,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 40,
+            },
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          {services.map(({ name, title, description }, index) => (
+            <SwiperSlide className={classes.servicesCard} key={index}>
+              <span className={classes.servicesSubtitle}>{name}</span>
+              <h3 className={classes.servicesTitle}>{title}</h3>
+              <p className={classes.servicesDescription}>{description}</p>
+              <h4 className={classes.free}>free</h4>
+              <img
+                src={shapeTwo}
+                alt="shape image"
+                className={classes.servicesShape}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </section>
+    </div>
   );
 };
 export default Services;
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -3,33 +3,38 @@ import { skills } from "../../assets/data/data";
 const Skills = () => {
   const container = "container";
   return (
-    <section className={`${container} ${classes.skillsSection}`} id="services">
-      <h2 className="sectionTitle">Professional Skills</h2>
-      <p className="sectionSubtitle">
-        My <span>Talent</span>
-      </p>
-      <div className={classes.skillsContainer}>
-        {skills.map(({ name, percentage, description }, index) => (
-          <div className={classes.skillsItem} key={index}>
-            <div className={classes.skillsTitle}>
-              <h3 className={classes.skillsName}>{name}</h3>
-              <span className={classes.skillsNumber}>
-                {percentage} <span>%</span>
-              </span>
+    <div className={classes.skillsPage}>
+      <section
+        className={`${container} ${classes.skillsSection}`}
+        id="services"
+      >
+        <h2 className="sectionTitle">Professional Skills</h2>
+        <p className="sectionSubtitle">
+          My <span>Talent</span>
+        </p>
+        <div className={classes.skillsContainer}>
+          {skills.map(({ name, percentage, description }, index) => (
+            <div className={classes.skillsItem} key={index}>
+              <div className={classes.skillsTitle}>
+                <h3 className={classes.skillsName}>{name}</h3>
+                <span className={classes.skillsNumber}>
+                  {percentage} <span>%</span>
+                </span>
+              </div>
+              <p className={classes.skillsDescription}>{description}</p>
+              <div className={classes.skillsBar}>
+                <span
+                  className={classes.skillsPercentage}
+                  style={{ width: `${percentage}%` }}
+                >
+                  <span></span>
+                </span>
+              </div>
             </div>
-            <p className={classes.skillsDescription}>{description}</p>
-            <div className={classes.skillsBar}>
-              <span
-                className={classes.skillsPercentage}
-                style={{ width: `${percentage}%` }}
-              >
-                <span></span>
-              </span>
-            </div>
-          </div>
-        ))}
-      </div>
-    </section>
+          ))}
+        </div>
+      </section>
+    </div>
   );
 };
 export default Skills;
