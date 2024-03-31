b46e3f03bc3c04816af47c42c0beaa16f21ef63d
Olena-Olkhovyk <alyonaeremen@gmail.com>
2024-03-31 19:53:11 +0200

    Add functionality to recieve message

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
 import classes from "./contact.module.css";
 
 import { FaRegAddressBook, FaRegEnvelope, FaRegMap } from "react-icons/fa";
+import axios from "axios";
 const Contact = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(
+        "https://sheet.best/api/sheets/b5262895-c463-456b-8ddc-67cac6a675dd",
+        form
+      )
+      .then((response) => {
+        console.log(response);
+        //clearing from fileds
+        setForm({ name: "", email: "", subject: "", message: "" });
+      });
+  };
   const container = "container";
   return (
     <div className={classes.contactPage}>
@@ -35,32 +63,55 @@ const Contact = () => {
               <p className={classes.cardData}>+4915121380883</p>
             </div>
           </div>
-          <form className={classes.contactForm}>
+          <form className={classes.contactForm} onSubmit={handleSubmit}>
             <div className={classes.formGroup}>
               <div className={classes.formDiv}>
                 <label className={classes.formTag}>
                   Your Name <b>*</b>
                 </label>
-                <input type="text" className={classes.formInput} />
+                <input
+                  type="text"
+                  name="name"
+                  value={form.name}
+                  onChange={handleChange}
+                  className={classes.formInput}
+                />
               </div>
               <div className={classes.formDiv}>
                 <label className={classes.formTag}>
                   Your Email <b>*</b>
                 </label>
-                <input type="email" className={classes.formInput} />
+                <input
+                  type="email"
+                  name="email"
+                  value={form.email}
+                  onChange={handleChange}
+                  className={classes.formInput}
+                />
               </div>
             </div>
             <div className={classes.formDiv}>
               <label className={classes.formTag}>
                 Your Subject <b>*</b>
               </label>
-              <input type="text" className={classes.formInput} />
+              <input
+                type="text"
+                name="subject"
+                value={form.subject}
+                onChange={handleChange}
+                className={classes.formInput}
+              />
             </div>
             <div className={`${classes.formDiv} ${classes.formArea}`}>
               <label className={classes.formTag}>
                 Your Message <b>*</b>
               </label>
-              <textarea className={classes.formInput}></textarea>
+              <textarea
+                className={classes.formInput}
+                name="message"
+                value={form.message}
+                onChange={handleChange}
+              ></textarea>
             </div>
 
             <div className={classes.submit}>
