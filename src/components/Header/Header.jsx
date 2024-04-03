d4b4848b83cb6de6af214088e64b31ebf60b4bf3
Olena-Olkhovyk <alyonaeremen@gmail.com>
2024-04-03 20:04:09 +0200

    Add functionality for link scrolling and header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,27 +3,57 @@ import { links } from "../../assets/data/data";
 import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 import { BsSun, BsMoon } from "react-icons/bs";
 import { useEffect, useState } from "react";
+import { Link } from "react-scroll";
+import { animateScroll } from "react-scroll";
 const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const scrollTop = () => {
+    animateScroll.scrollToTop();
+  };
+
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNav);
+  }, []);
 
   useEffect(() => {
     document.body.classList.toggle("no-scroll", showMenu);
   }, [showMenu]);
   return (
-    <header className={classes.header}>
+    <header
+      className={`${classes.header} ${scrollNav ? classes.scrollHeader : ""} `}
+    >
       <nav className={classes.nav}>
-        <a href="" className={classes.navLogo}>
+        <Link to="/" onClick={scrollTop} className={classes.navLogo}>
           Olkhovyk
-        </a>
+        </Link>
 
         <div className={showMenu ? classes.showMenu : classes.navMenu}>
           <div className={classes.navData}>
             <ul className={classes.navList}>
               {links.map(({ name, path }, index) => (
                 <li className={classes.navItem} key={index}>
-                  <a href="" className={classes.navLink}>
+                  <Link
+                    className={classes.navLink}
+                    to={path}
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                    onClick={() => setShowMenu(!showMenu)}
+                    activeClassName={classes.active}
+                  >
                     {name}
-                  </a>
+                  </Link>
                 </li>
               ))}
             </ul>
